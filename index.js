ad76c89d19ca137d550576c7c57d579f4597e622
AnujNigam12 <anujnig.07@gmail.com>
2025-04-28 11:56:24 +0530

    Version1

--- index.js
+++ index.js
@@ -16,17 +16,15 @@ const allowedOrigins = [process.env.FRONTEND_URL]; // Add more origins as needed
 
 app.use(
     cors({
-        origin: function (origin, callback) {
-            if (!origin || allowedOrigins.includes(origin)) {
-                callback(null, true);
-            }
-            else {
-                callback(new Error('Not allowed by CORS'));
-            }
-        },
-        credentials: true
-    })
-)
+        origin: [
+            'http://localhost:5173',
+            allowedOrigins
+        ],
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE"]
+    }
+));
+
 app.use(bodyParser.json());
 app.use(cookieParser({
     httpOnly: true,
